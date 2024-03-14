adfb6832b72f0a99d35dc8cc7050932754d72cc7
BSY <45937750+ideawolf@users.noreply.github.com>
2024-03-14 11:44:13 +0900

    base

--- team-page/src/App.js
+++ team-page/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // 스타일시트 임포트
+import logo from './logo.png'; // 로고 이미지 임포트
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <img src={logo} alt="Logo" className="logo" />
+        {/* 여기에 다른 컴포넌트나 내용 추가 */}
+      </div>
     </div>
   );
 }
