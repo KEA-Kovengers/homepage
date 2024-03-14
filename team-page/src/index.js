ca6441a7e00b2fa61942b3219c9db84a9edc0d36
ParkHyunseo <sspp8585@icloud.com>
2024-03-14 20:59:04 +0900

    feat: add personal information for Hyunseo, implement scroll to top feature for page transitions

--- team-page/src/index.js
+++ team-page/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import ScrollTop from "./ScrollTop";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
+);
+root.render(
+  <BrowserRouter>
+    <ScrollTop />
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
