479d8f04f276b388af47db3e4b5484f04ab7da4e
Hyewon Yoon <105503671+iey704@users.noreply.github.com>
2024-03-14 19:01:18 +0900

    chore: add hyewon information

--- team-page/src/App.js
+++ team-page/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Route path="/miso" element={<MemberPage profile={misoProfile} name='Miso Kim' email='althcjstk08@gachon.ac.kr' github='bamb14' githubLink='https://github.com/bamb14' phone='010-7930-7725' role='frontend' birth='2002.09.08' mbti='INFP' address='서울특별시 동작구' />} />
       <Route path="/somi" element={<MemberPage profile={somiProfile} name='Somi Nam' email='somi4219@gachon.ac.kr' github='somi4219' githubLink='https://github.com/somi4219' phone='010-2312-5186' role='backend' birth='2002.10.23' mbti='ESTJ' address='경기도 안양시' />} />
       <Route path="/sangyeon" element={<MemberPage profile={sangyeonProfile} name='Sangyeon Byeon' email='' github='' githubLink='' phone='' role='' birth='' mbti='' address='' />} />
-      <Route path="/hyewon" element={<MemberPage profile={hyewonProfile} name='Hyewon Yoon' email='iey704@gachon.ac.kr' github='iey704' githubLink='https://github.com/iey704' phone='010-9107-6619' role='frontend' birth='2001.03.20' mbti='ISFP' address='서울특별시 강남구' />} />
+      <Route path="/hyewon" element={<MemberPage profile={hyewonProfile} name='Hyewon Yoon' email='iey704@gachon.ac.kr' github='iey704' githubLink='https://github.com/iey704' phone='010-9107-6619' role='frontend' birth='2001.03.20' mbti='ISFP' address='서울특별시 강남구 ' />} />
       <Route path="/sunghoon" element={<MemberPage profile={sunghoonProfile} name='Sunghoon Jung' email='' github='' githubLink='' phone='' role='' birth='' mbti='' address='' />} />
       <Route path="/hyunseo" element={<MemberPage profile={hyunseoProfile} name='Hyunseo Park' email='' github='' githubLink='' phone='' role='' birth='' mbti='' address='' />} />
     </Routes>
